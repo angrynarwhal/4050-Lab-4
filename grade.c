9f095006a247db3576c0ea4363d8c2fc35bb93b7
Addy8630 <aam9d@umsystem.edu>
2026-03-11 18:34:13 -0500

    grade.c

---
+++ grade.c
@@ -0,0 +1,11 @@
+// CMP_SC 4050
+// Fall 2024
+// A4 - "Bad Scantron"
+
+#include "grade.h"
+
+int Grade(int key[], int keyResponseCount, int exam[], int examResponseCount)
+{
+    // Implement your Grade() function here.  This one does nothing at the moment (just returns 0)
+    return 0;
+}
