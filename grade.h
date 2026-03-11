2d227e0f177520e78e8c7225763fb3df244c8ac8
Addy8630 <aam9d@umsystem.edu>
2026-03-11 18:34:58 -0500

    grade.h

---
+++ grade.h
@@ -0,0 +1,9 @@
+// CMP_SC 4050
+// Fall 2024
+// A4 - "Bad Scantron"
+
+#ifndef _GRADE_H
+#define _GRADE_H
+// Prototypes
+int Grade(int key[], int keyResponseCount, int exam[], int examResponseCount);
+#endif
